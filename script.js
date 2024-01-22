ecaaa6ebf219142a988d965056e5f2c141919f72
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-22 10:59:10 +0100

    Step 24

--- script.js
+++ script.js
@@ -90,6 +90,10 @@ const playSong = (id) => {
     const song = userData?.songs.find((song) => song.id === id);
     audio.src = song.src;
     audio.title = song.title;
+
+    if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+        audio.currentTime = 0;
+    }
 };
 
 const renderSongs = (array) => {
