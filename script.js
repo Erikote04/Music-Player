4053cd10aa0e0a62894e70a3ba48de080580da09
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-22 10:19:02 +0100

    Step 6

--- script.js
+++ script.js
@@ -20,4 +20,11 @@ const allSongs = [
         duration: "4:15",
         src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stay-down.mp3",
     },
+    {
+        id: 2,
+        title: "Still Learning",
+        artist: "Quincy Larson",
+        duration: "3:51",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/still-learning.mp3",
+    },
 ];
