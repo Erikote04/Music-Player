b0780aafe7482ef20daac7d41b73f37c0dc340da
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-22 11:07:25 +0100

    Step 30

--- script.js
+++ script.js
@@ -127,6 +127,8 @@ const renderSongs = (array) => {
 playButton.addEventListener("click", () => {
     if (userData?.currentSong === null) {
         playSong(userData?.songs[0].id);
+    } else {
+        playSong(userData?.currentSong.id);
     }
 });
 
