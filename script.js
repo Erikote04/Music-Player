0f8d978deac54589bb76ac4653567e503c13b874
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-22 10:37:44 +0100

    Step 14

--- script.js
+++ script.js
@@ -87,5 +87,8 @@ let userData = {
 };
 
 const renderSongs = (array) => {
-    const songsHTML = array.map((song) => {});
+    const songsHTML = array.map((song) => {
+        return `
+        <li id="song-${song.id}" class="playlist-song"></li>`;
+    });
 };
