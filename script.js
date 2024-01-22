ffacd6e946ddb2da4a3230e969926bd7f113e00a
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-22 10:45:32 +0100

    Step 16

--- script.js
+++ script.js
@@ -93,7 +93,10 @@ const renderSongs = (array) => {
             <li id="song-${song.id}" class="playlist-song">
                 <button class="playlist-song-info">
                     <span class="playlist-song-title">${song.title}</span>
+                    <span class="playlist-song-artist">${song.artist}</span>
+                    <span class="playlist-song-duration">${song.duration}</span>
                 </button>
-            </li>`;
+            </li>
+            `;
         });
 };
