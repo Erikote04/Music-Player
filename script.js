9cbd8bdbdb9a82ca37217d49cdd92e774a9457ea
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-23 09:54:36 +0100

    Step 41

--- script.js
+++ script.js
@@ -115,6 +115,8 @@ const playNextSong = () => {
         playSong(userData?.songs[0].id);
     } else {
         const currentSongIndex = getCurrentSongIndex();
+        const nextSong = userData?.songs[currentSongIndex + 1];
+        playSong(nextSong.id);
     }
 };
 
