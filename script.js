ed29c9c3096a684101d91a22f319a92bb6550a92
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-23 10:56:38 +0100

    Step 69

--- script.js
+++ script.js
@@ -147,6 +147,9 @@ const shuffle = () => {
 
 const deleteSong = (id) => {
     userData.songs = userData?.songs.filter((song) => song.id !== id);
+    renderSongs(userData?.songs);
+    highlightCurrentSong();
+    setPlayButtonAccessibleText();
 };
 
 const setPlayerDisplay = () => {
