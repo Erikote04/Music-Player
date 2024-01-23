efc384be721b048dba7e51c8cd330ea4ba5d6499
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-23 10:49:19 +0100

    Step 65

--- script.js
+++ script.js
@@ -138,6 +138,11 @@ const shuffle = () => {
     userData?.songs.sort(() => Math.random() - 0.5);
     userData.currentSong = null;
     userData.songCurrentTime = 0;
+
+    renderSongs(userData?.songs);
+    pauseSong();
+    setPlayerDisplay();
+    setPlayButtonAccessibleText();
 };
 
 const setPlayerDisplay = () => {
