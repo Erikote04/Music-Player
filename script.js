5cdde540b32d23c4c07dc547e0eee56a75dac20d
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-23 10:18:36 +0100

    Step 54

--- script.js
+++ script.js
@@ -133,7 +133,10 @@ const playNextSong = () => {
     }
 };
 
-const setPlayerDisplay = () => {};
+const setPlayerDisplay = () => {
+    const playingSong = document.getElementById("player-song-title");
+    const songArtist = document.getElementById("player-song-artist");
+};
 
 const highlightCurrentSong = () => {
     const playlistSongElements = document.querySelectorAll(".playlist-song");
