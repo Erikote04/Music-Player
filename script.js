a1b901f52ea0fa29a11ea92c1ebf8f3c7c45ecca
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-23 10:52:02 +0100

    Step 67

--- script.js
+++ script.js
@@ -145,6 +145,8 @@ const shuffle = () => {
     setPlayButtonAccessibleText();
 };
 
+const deleteSong = (id) => {};
+
 const setPlayerDisplay = () => {
     const playingSong = document.getElementById("player-song-title");
     const songArtist = document.getElementById("player-song-artist");
