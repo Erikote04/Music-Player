00e69135702b2bcf8af99eeb66810123d3d2a58c
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-23 10:26:33 +0100

    Step 56

--- script.js
+++ script.js
@@ -138,6 +138,9 @@ const setPlayerDisplay = () => {
     const songArtist = document.getElementById("player-song-artist");
     const currentTitle = userData?.currentSong?.title;
     const currentArtist = userData?.currentSong?.artist;
+
+    playingSong.textContent = currentTitle ? currentTitle : "";
+    songArtist.textContent = currentArtist ? currentArtist : "";
 };
 
 const highlightCurrentSong = () => {
