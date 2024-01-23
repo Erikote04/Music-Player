d74362e4319b0e753242a92855a8a865f1cacfca
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-23 11:05:30 +0100

    Step 75

--- script.js
+++ script.js
@@ -160,6 +160,7 @@ const deleteSong = (id) => {
 
     if (userData.songs.length === 0) {
         const resetButton = document.createElement("button");
+        const resetText = document.createTextNode("Reset Playlist");
     }
 };
 
