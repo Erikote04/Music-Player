6c789d635d79e7fe06fb11d9f580f1fe54243a76
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-30 12:37:38 +0100

    Step 89

--- script.js
+++ script.js
@@ -266,6 +266,10 @@ userData?.songs.sort((a, b) => {
     if (a.title < b.title) {
         return -1;
     }
+
+    if (a.title > b.title) {
+        return 1;
+    }
 });
 renderSongs(userData?.songs);
 setPlayButtonAccessibleText();
