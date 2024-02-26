8c98bb67e7ff1721584db77a9a4c4596f6d21fc5
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-02-26 09:58:25 +0100

    Step 91

--- script.js
+++ script.js
@@ -262,17 +262,20 @@ audio.addEventListener("ended", () => {
     }
 });
 
-userData?.songs.sort((a, b) => {
-    if (a.title < b.title) {
-        return -1;
-    }
+const sortSongs = () => {
+    userData?.songs.sort((a, b) => {
+        if (a.title < b.title) {
+            return -1;
+        }
+    
+        if (a.title > b.title) {
+            return 1;
+        }
+    
+        return 0;
+    });
+    return userData?.songs;
+}
 
-    if (a.title > b.title) {
-        return 1;
-    }
-
-    return 0;
-});
-
-renderSongs(userData?.songs);
+renderSongs(sortSongs());
 setPlayButtonAccessibleText();
